3c4a08c6637711a6c8e73e7dd03dd6984b8f5e56
soufiancode <155249756+soufiancode@users.noreply.github.com>
2024-02-08 03:10:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+﻿// JavaScript code
+document.getElementById("myButton").addEventListener("click", function() {
+    alert("Button clicked!");
+    // You can add more JavaScript code here to handle the button click event
+});
